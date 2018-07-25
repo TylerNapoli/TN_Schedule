b096745b6bb08c603309bd9de0a3ad860cc411f7
Code <Code@batc.local>
2018-07-25 10:15:43 -0600

    made a new folder on local

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 
 import Icon from '../icons';
 import Arrow from '../arrow';
+import Action from '../action';
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
-                <div className="library-course__title-arrow">
+                <div className="library-course__title-check">
                     <label className="library-course__title">Problem Solving</label>
                     {Icon('fas fa-check', 'library-course__icon')}
                 </div>
-                <Arrow className=";ibrary-course__arrow" />
-                {/* action button component */}
+                <Arrow className="library-course__arrow" />
+                <Action className="library-course__action" />
                 <div className="library-course__discription">
                     <label>Course Discription</label>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque scelerisque augue ut est tempor bibendum. Donec at diam auctor, vehicula leo sit amet, placerat nibh. Vestibulum sit amet erat non ex sollicitudin volutpat id blandit odio.</p>
